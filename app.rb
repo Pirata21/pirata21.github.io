8630824c1baeced6dfa950bc9e75630169d93ba2
Martin Prado <islamaldita1@gmail.com>
2015-06-15 12:17:46 -0300

    new domain

---
+++ app.rb
@@ -0,0 +1,11 @@
+before do
+	cache_control :public, :max_age => 31557600
+end
+ 
+get '/' do
+	send_file(File.join(settings.public, 'index.html'), :disposition => nil)
+end
+ 
+get '/*' do
+	send_file(File.join(settings.public, params[:splat]), :disposition => nil)
+end
